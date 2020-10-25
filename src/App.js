4cb31989ef85ca68c7d427e4cb6c317dbdc515d5
ClausR1990 <claus.reinhold@gmail.com>
2020-10-25 22:15:09 +0100

    Added Router component for make navigation in the app possible

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-
 import HomePage from './views/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
