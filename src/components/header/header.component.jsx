f9ae210b0f169f7261a6078714a54923b378c78a
ClausR1990 <claus.reinhold@gmail.com>
2020-10-28 19:30:57 +0100

    Added Redux with user reducer and action component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.styles.scss';
+import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
 import { auth } from '../../firebase/firebase.utils';
+
+
+import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
     <div className="header">
@@ -21,6 +23,10 @@ const Header = ({ currentUser }) => (
             }
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
